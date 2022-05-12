e7e835f4b203df482ef9d4b8520e3d5b8ece11b2
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-12 18:47:51 -0300

    Jogatina UFPR

---
+++ Iniciante/URI 2543.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+
+      Scanner sc=new Scanner(System.in);
+      while(sc.hasNext()){
+         int number,id;
+         number=sc.nextInt();
+         id=sc.nextInt();
+         int count=0;
+         while(number>0){
+           int id1,num;
+           id1=sc.nextInt();
+           num=sc.nextInt();
+           if(id==id1&&num==0)
+              count++;
+           number--;
+         }
+         System.out.println(count);
+     }
+   }
+}
