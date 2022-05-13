aa47c2083fc66068166c8ec6cd328a688fb25c28
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 00:12:45 -0300

    Raiz Quadrada de 10

---
+++ Iniciante/URI 2161.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+    	Scanner leitor = new Scanner(System.in);
+    	double N = leitor.nextInt();
+        double x = 0.0;
+    	for (int i = 0; i < N; i++) {
+            x += 6.0;
+            x = (1.0 / x);
+    	}
+        x += 3.0;
+    	System.out.println(String.format("%.10f", x));
+    }
+	
+}
