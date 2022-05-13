a6df0282b95c962084eee8eb677d397be52af458
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 09:08:18 -0300

    Revisão de Contrato

---
+++ Strings/URI 1120.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) throws IOException {
+		String l;
+		while (!(l = in.readLine()).equals("0 0")) {
+			String[] p = l.split("\\s");
+			String D = p[0];
+			String N = p[1];
+			String r = N.replace(D, "");
+			r = r.replaceFirst("0*", "");
+			System.out.println(r.length() > 0 ? r : 0);
+		}
+		out.close();
+	}
+}
