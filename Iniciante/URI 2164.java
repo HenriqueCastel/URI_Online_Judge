ae9c391cc967309afc2f1924d911a5e85ac8de7f
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 00:09:53 -0300

    Fibonacci Rápido

---
+++ Iniciante/URI 2164.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+    	Scanner leitor = new Scanner(System.in);
+    	double N = leitor.nextInt();
+    	double fib = (Math.pow((1 + Math.sqrt(5)) / 2, N) - Math.pow((1 - Math.sqrt(5)) / 2, N)) / Math.sqrt(5);
+    	System.out.println(String.format("%.1f", fib));
+    }
+	
+}
