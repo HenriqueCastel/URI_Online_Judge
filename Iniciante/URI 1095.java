347614611c9bcc41c2dd4bd43af229f0c160ff8a
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 14:07:35 -0300

    Sequencia IJ 1

---
+++ Iniciante/URI 1095.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        for(int i= 1, j= 60; j >= 0; i += 3, j -= 5) {
+            System.out.println("I=" + i + " J=" + j);
+        }
+    }
+ 
+}
