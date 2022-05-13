5483417e1ad0c9c7008d082be5156c095cc72df8
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-12 23:46:14 -0300

    Batmain

---
+++ Iniciante/URI 2510.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+		Scanner leitor = new Scanner(System.in);
+		int T = leitor.nextInt();
+		for (int i = 0; i < T; i++) {
+			String vilao = leitor.nextLine();
+			System.out.println("Y");
+		}
+	}
+	
+}
