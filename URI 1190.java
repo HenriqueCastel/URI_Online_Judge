0d0cc983a0121f381cd9f4cf2230565fa0725a4d
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 09:10:00 -0300

    Área Direita

---
+++ URI 1190.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+		Scanner t = new Scanner(System.in);
+		double soma = 0;
+		char O = t.next().toUpperCase().charAt(0);
+		double M[][] = new double[12][12];
+		for(int i = 0; i < M.length; i++) {
+			for(int j = 0; j < M[i].length; j++) {
+				M[i][j] = t.nextDouble();
+			}
+		}
+		for(int i = 0; i < M.length; i++) {
+			for(int j = 0; j < M[i].length; j++){
+				if(j > i && j > M.length - 1 - i) {
+					soma += M[i][j];
+				}
+			}
+		}
+		if(O == 'M') {
+			soma /= (M.length * M.length - 24) / 4;
+		}
+		System.out.printf("%.1f\n", soma);
+		t.close();
+	}
+
+}
