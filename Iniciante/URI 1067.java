ed28e4a7ea877ff22b27e9d080b6b7d5e8265681
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 14:18:29 -0300

    Números Ímpares

---
+++ Iniciante/URI 1067.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner t= new Scanner(System.in);
+        int x= t.nextInt();
+        for(int i= 1; i <= x; i++) {
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+        t.close();
+    }
+ 
+}
