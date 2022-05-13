6a4d6d6ebedaae533041f7d2fa5b9f9fd67769a4
HenriqueCastel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 00:09:16 -0300

    Tuitando

---
+++ Iniciante/URI 2165.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.ENGLISH);
+		Locale.setDefault(new Locale("en", "US"));
+		
+		String twit = sc.nextLine();
+		if (twit.length()<=140)
+			System.out.println("TWEET");
+		else
+			System.out.println("MUTE");
+
+		sc.close();
+	}
+}
