2926797ce8d6db01442d3be1bc4f5efc0d25f53f
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-13 23:10:53 -0300

    Notação Científica

---
+++ Iniciante/URI 1958.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+    	Scanner leitor = new Scanner(System.in);
+		double X = leitor.nextDouble();
+		System.out.println(String.format((String.valueOf(X).startsWith("-") ? "" : "+") + "%.4E", X));
+    }
+	
+}
