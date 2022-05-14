b22caec558b6b5fa3a7df8bd1507ef96ee1f3477
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-05-14 00:04:49 -0300

    Intervalo

---
+++ Iniciante/URI 1037.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.IOException;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        float N;
+        Scanner t= new Scanner(System.in);
+        N= t.nextFloat();
+        if (N >= 0 && N <= 25.0000) {
+            System.out.printf("Intervalo [0,25]\n");
+        }else if (N >= 25.00001 && N <= 50.0000000) {
+            System.out.printf("Intervalo (25,50]\n");
+        }else if (N >= 50.00001 && N <= 75.0000000) {
+            System.out.printf("Intervalo (50,75]\n");
+        }else if (N >= 75.00001 && N<=100.0000000) {
+            System.out.printf("Intervalo (75,100]\n");
+        }else {
+            System.out.print("Fora de intervalo\n");
+            t.close();
+        }
+
+    }
+
+}
